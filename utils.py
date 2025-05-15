a0e555831a3ee5afd916de4426f99d971646b154
egerc <153100377+egerc@users.noreply.github.com>
2025-05-15 09:53:56 +0200

    .

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Generator
+from typing import Any, Callable, Generator, Optional
 
 import numpy as np
 from numpy.typing import NDArray
@@ -10,11 +10,16 @@ def get_aligned_gene_sets(
 ) -> tuple[NDArray[np.str_], NDArray[np.str_]]:
     """
     Identifies gene subsets from query and reference gene indices.
+    Necessary to order the count matrixes feature wise for prediction.
+
+    Arguments:
+        query_genes: Index of gene names measured in the query data
+        reference_genes: Index of gene names measured in the reference data
 
     Returns:
-    - shared_genes: genes common to both query and reference, in sorted order.
-    - reference_gene_order: genes in reference composed of shared genes
-      followed by reference-specific genes (both sorted).
+        shared_genes: array of gene names common to both query and reference, in sorted order.
+        reference_gene_order: array of gene names in reference composed of shared genes
+        followed by reference-specific genes (both sorted).
     """
     shared_genes = np.intersect1d(query_genes, reference_genes)
     reference_only_genes = np.setdiff1d(reference_genes, shared_genes)
@@ -46,7 +51,7 @@ def cross_feature_prediction(
 
     Assumes that features are aligned between query and reference matrices.
 
-    Parameters:
+    Arguments:
         query_matrix: Array of shape (n_samples, n_features).
         reference_matrix: Array of shape (n_reference_samples, n_features).
         predictor: A function that takes (query_submatrix, reference_submatrix)
@@ -73,12 +78,13 @@ def mean_randomized_cross_feature_prediction(
     reference_matrix: NDArray,
     predictor: Callable[[NDArray, NDArray], NDArray],
     iterations: int,
+    seed: Optional[int] = None,
 ) -> NDArray:
     """
     Applies feature-wise cross-prediction multiple times with random feature permutations,
     then returns the mean prediction.
 
-    Parameters:
+    Arguments:
         query_matrix: Array of shape (n_samples, n_features), with query observations.
         reference_matrix: Array of shape (n_reference_samples, n_features), with reference data.
         predictor: A function that takes (query_submatrix, reference_submatrix) and returns predicted features.
@@ -89,7 +95,7 @@ def mean_randomized_cross_feature_prediction(
     Returns:
         Array of shape (n_samples, n_features) containing the averaged predictions.
     """
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(seed=seed)
     res = []
     for _ in range(iterations):
         feature_order: NDArray[np.int64] = rng.permutation(np.arange(query_matrix.shape[1]))
